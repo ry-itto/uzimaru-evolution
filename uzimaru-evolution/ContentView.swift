aa01e8f03ce83624ca39f1e18c1d6d6e2d7c4057
ryoya ito <110ryoya@gmail.com>
2020-05-27 14:52:16 +0900

    :art: introduce core assets

--- uzimaru-evolution/ContentView.swift
+++ uzimaru-evolution/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color("background")
+            Text("Hello, World!")
+                .foregroundColor(Color("uzimaru"))
+        }
     }
 }
 
