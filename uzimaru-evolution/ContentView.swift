e9258cdc71cf25522c82dc961772433422e3a76d
ryoya ito <110ryoya@gmail.com>
2020-05-27 16:05:48 +0900

    :art: view done

--- uzimaru-evolution/ContentView.swift
+++ uzimaru-evolution/ContentView.swift
@@ -12,8 +12,38 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color("background")
-            Text("Hello, World!")
-                .foregroundColor(Color("uzimaru"))
+            VStack {
+                Text("uzimaru Evolution")
+                    .foregroundColor(Color("uzimaru"))
+                    .font(Font.largeTitle.weight(.black))
+                    .underline(true, color: Color.white.opacity(0.5))
+                Image("v1")
+                    .resizable()
+                    .scaledToFit()
+                HStack(spacing: 30) {
+                    Button(
+                        action: {
+
+                        },
+                        label: {
+                            Image(systemName: "arrow.left.circle")
+                                .font(Font.title.weight(.bold))
+                                .foregroundColor(Color("uzimaru"))
+                        })
+                    Text("v1")
+                        .font(Font.largeTitle.weight(.heavy))
+                        .foregroundColor(Color("uzimaru"))
+                    Button(
+                        action: {
+
+                        },
+                        label: {
+                            Image(systemName: "arrow.right.circle")
+                                .font(Font.title.weight(.bold))
+                                .foregroundColor(Color("uzimaru"))
+                        })
+                }
+            }
         }
     }
 }
